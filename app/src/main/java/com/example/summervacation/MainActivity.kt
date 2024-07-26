e6d2edcf7a434e399335145e0e55431075dcd292
Tai E <1607869392@qq.com>
2024-07-26 23:06:00 +0800

    实现了开局广告的功能

--- app/src/main/java/com/example/summervacation/MainActivity.kt
+++ app/src/main/java/com/example/summervacation/MainActivity.kt
@@ -1,18 +1,47 @@
 package com.example.summervacation
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.alibaba.android.arouter.launcher.ARouter
+import com.example.module.main.databinding.ActivityMainBinding
+import com.example.summervacation.databinding.ActivityMainsappBinding
 
 class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainsappBinding.inflate(layoutInflater)
+    }
+    private val adDuration: Long = 5000 // 5 seconds
+    private val handler = Handler(Looper.getMainLooper())
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_mainsapp)
+        setContentView(binding.root)
+        binding.skipButton.setOnClickListener {
+            navigateToMain()
+        }
+        // Delay to navigate to main activity
+        handler.postDelayed({
+            navigateToMain()
+        }, adDuration)
+    }
+
+    private fun navigateToMain() {
+        if (!isFinishing) {
+            handler.removeCallbacksAndMessages(null)
             ARouter.getInstance().build("/login/LoginActivity")
                 .navigation()
+            finish()
         }
+    }
+
+
+
+
+
+
 
     }
 
